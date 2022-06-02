ad449ba65213b02bb2bcd21ee80e770941d1a794
Dhiraj Kumar Jha <callmedhiraj@gmail.com>
2022-06-03 00:36:28 +0545

    The database connection has been added

--- mytest.php
+++ mytest.php
@@ -1,3 +1,11 @@
 <?php 
 echo phpinfo();
+//connect to the database
+$connection = mysqli_connect("localhost", "root", "","classicmodels");
+if($connection != NULL){
+    echo "The database has been connected";
+}
+else{
+    echo "There is some issue with the database";
+}
 ?>
